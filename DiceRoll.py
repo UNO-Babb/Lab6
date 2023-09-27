65f7f66b7129ee2a6aa0991777be39be7d9dca86
Derek Babb <dbabb1@gmail.com>
2023-09-27 09:07:56 -0500

    Update DiceRoll.py

--- DiceRoll.py
+++ DiceRoll.py
@@ -5,13 +5,13 @@
 import random
 
 def main():
-    #Create an empty list with possible roll values
-    rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
-    #Create two dice values ranging from 1 - 6 each
-
-    #find the sum total of the two dice
-
-    #print statictics for dice rolls
+  #Create an empty list with possible roll values
+  rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
+  #Create two dice values ranging from 1 - 6 each
+  
+  #find the sum total of the two dice
+  
+  #print statictics for dice rolls
 
 
 if __name__ == '__main__':
